667520878b9df7a53ef80c0589b95ab17c921df9
Ilya <nekrasov@greensight.ru>
2016-06-04 18:55:04 +0300

    Drop illuminate/support

---
+++ src/Helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Arrilot\BitrixMigrations;
+
+class Helpers
+{
+    /**
+     * Convert a value to studly caps case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function studly($value)
+    {
+        $value = ucwords(str_replace(['-', '_'], ' ', $value));
+
+        return str_replace(' ', '', $value);
+    }
+}
